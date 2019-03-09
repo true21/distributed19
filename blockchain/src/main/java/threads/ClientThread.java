e7b40cb944a8c56c81e8f412cff3928c7250d8f7
true21 <drewgriz@gmail.com>
2019-03-09 11:44:41 +0200

    some

--- blockchain/src/main/java/threads/ClientThread.java
+++ blockchain/src/main/java/threads/ClientThread.java
@@ -1,55 +1,111 @@
-package threads;
+//package threads;
 
-import beans.Message;
-import beans.MessageType;
-import entities.NodeMiner;
-import entities.Transaction;
+//import beans.Message;
+//import beans.MessageType;
+//import entities.NodeMiner;
+//import entities.Transaction;
 
 import javax.sound.sampled.LineEvent;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.net.InetAddress;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Scanner;
 
 public class ClientThread extends Thread {
-	
-	private InetAddress ipAddress;    
-	private int index;
-	private Socket socket;  
+
+	private InetAddress ipAddress;
+	private static int index;
+	private Socket socket;
 	private int port;
-	
-	public ClientThread(InetAddress i, int c, Socket sock, int prt){ 
-		ipAddress = i; 
-		index = c;  
+
+	public ClientThread(InetAddress i, int c, Socket sock, int prt) {
+		ipAddress = i;
+		index = c;
 		socket = sock;
-		int port = prt;
+		port = prt;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-        System.out.println("Enter the IP address of a machine running the date server:");
-        Socket soct = new Socket(InetAddress.getByName(args[0]), 9090, InetAddress.getByName(args[0]), args[1]); //176.58.247.145 or 127.0.0.1 ???, port = 1000 ++
-		int cnt = args[1]-1000; //index
-		System.out.println("Spawning " + cnt);
-        Thread t = new ClientThread(InetAddress.getByName(args[0]), cnt, soct, args[1]);
-        t.start();
-    }
+    try {
 
-    /**
-     * Add data to sent to a Message object
-     * @return the Message
-     */
-    private Message createMessage() {
-        return null;
-    }
+			// socket
+			InetAddress ip = InetAddress.getByName(args[0]);
+			Socket s = new Socket(ip, 9090 + index);
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+      DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			// get input from console
+			Scanner scanner = new Scanner(System.in);
 
-    @Override
-    public void run() {
-		//create new nodeminer , genesis block 
-		BufferedReader in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
-		String str = in.readLine();
-    }
+			// file
+			String file = "transactions" + index + ".txt";
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+  		while ((line = br.readLine()) != null) {
+				dos.writeUTF("t " + line.replace("id", ""));
+				String received = dis.readUTF();
+				System.out.println(received);
+  		}
+
+			// cli
+			while (true) {
+				String sendToServer = new String("");
+				String help = "explain stuff";
+				String error = "Bad syntax.";
+				// read line
+				String command = scanner.nextLine();
+				List<String> cmd_args = Arrays.asList(command.trim().split("\\s+"));
+				if (cmd_args.size() == 1) {
+					if (cmd_args.get(0).equals("view")) {
+						sendToServer = "view";
+					}
+					else if (cmd_args.get(0).equals("balance")) {
+						sendToServer = "balance";
+					}
+					else if (cmd_args.get(0).equals("help")) {
+						sendToServer = "help";
+					}
+				}
+				else if (cmd_args.size() == 3) {
+					if (cmd_args.get(0).equals("t")) {
+						sendToServer = cmd_args.get(0) + " " + cmd_args.get(1) + " " + cmd_args.get(2);
+					}
+				}
+				if (sendToServer.equals("")) {
+					System.out.println(error);
+					System.out.println(help);
+				}
+				else if (sendToServer.equals("help")) {
+					System.out.println(help);
+				}
+				else {
+					System.out.println("Send to Server: " + sendToServer);
+					dos.writeUTF(sendToServer);
+					String received = dis.readUTF();
+          System.out.println(received);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+  }
+/*
+  /**
+   * Add data to sent to a Message object
+   * @return the Message
+   */ /*
+  private Message createMessage() {
+  	return null;
+  }
+
+  @Override
+  public void run() {
+		//create new nodeminer , genesis block
+		System.out.println("AEK");
+  }
+*/
 }
--- blockchain/src/main/java/threads/ServerThread.java
+++ blockchain/src/main/java/threads/ServerThread.java
@@ -14,49 +14,48 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ServerThread extends Thread {
-	
-	private Socket incoming;    
+
+	private Socket incoming;
 	private int counter;
-	
-	public ServerThread(Socket i, int c){ 
-		incoming = i; 
-		counter = c;  
+
+	public ServerThread(Socket i, int c){
+		incoming = i;
+		counter = c;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-        try (ServerSocket listener = new ServerSocket(9090)) {
-            System.out.println("Server is running");
+		ServerSocket listener = new ServerSocket(9090);
+		try {
+    	System.out.println("Server is running");
 			int i = 0;
-            while (true) {
-                Socket socket = listener.accept();
-                System.out.println("Spawning " + i);
-                Thread t = new ServerThread(socket, i);
-                t.start();
-                i++;      
-            }
-		}  catch (Exception e) { e.printStackTrace(); }
-    }
-
-
-    /**
-     * Handle an incoming message
-     * @param Message msg
-     */
-    private void handleMessage(Message msg) {
-    }
-
-    @Override
-    public void run() {
-		{  try
-           { 
-			  ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Message.txt")); //dont have a clue
-              while (true){
-                  handleMessage((Message) ois.readObject());    
-              }
-			  ois.close();
-              incoming.close();
-          } catch (Exception e){  e.printStackTrace();  }
+      while (true) {
+	      Socket socket = listener.accept();
+	      System.out.println("Spawning " + i);
+	      Thread t = new ServerThread(socket, i);
+	      t.start();
+	      i++;
       }
-    }
-}
+		} catch (Exception e) { e.printStackTrace(); }
+  }
 
+
+  /**
+   * Handle an incoming message
+   * @param Message msg
+   */
+  private void handleMessage(Message msg) {
+  }
+
+  @Override
+  public void run() {
+	try {
+	  ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Message.txt")); //dont have a clue
+   	while (true){
+    	handleMessage((Message) ois.readObject());
+    }
+	  ois.close();
+	  incoming.close();
+	  } catch (Exception e){  e.printStackTrace();  }
+	}
+
+}
